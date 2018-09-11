84e850a8e63d1973aaa2c5df5446832bf210d4da
Unknown <iamyugachang@gmail.com>
2018-09-11 23:44:51 +0800

    test

--- hello.c
+++ hello.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int num = 0;
-    printf("hello !\n");
+    printf("hello 哈囉 !\n");
 
 
     
     
+    char command[50];
 
+   strcpy( command, "ls -l" );
+   system(command);
     return 0;
 }
